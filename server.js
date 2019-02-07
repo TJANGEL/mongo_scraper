af66ab8a694870b62398a47954edd28454948b38
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-02-07 08:46:34 -0500

    updated to server.js

--- server.js
+++ server.js
@@ -82,19 +82,56 @@ app.get("/saved", function(req, res) {
 // A GET request to scrape the echojs website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  request("https://tutorialzine.com/articles", function(error, response, html) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(html);
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("div.article__description").each(function(i, element) {
+  // request("https://tutorialzine.com/articles", function(error, response, html) {
+  //   // Then, we load that into cheerio and save it to $ for a shorthand selector
+  //   var $ = cheerio.load(html);
+  //   // Now, we grab every h2 within an article tag, and do the following:
+  //   $("div.article__description").each(function(i, element) {
 
-      // Save an empty result object
+  //     // Save an empty result object
+  //     var result = {};
+
+  //     // Add the title and summary of every link, and save them as properties of the result object
+  //     // result.title = $(this).children("h2").text();
+  //     // result.summary = $(this).children(".summary").text();
+  //     // result.link = $(this).children("h2").children("a").attr("href");
+  //     result.title = $(element)
+  //       .find("h3")
+  //       .text();
+  //     result.link = $(element)
+  //       .find("a")
+  //       .attr("href");
+  //     result.summary = $(element)
+  //       .find("p")
+  //       .text();
+
+  //     // Using our Article model, create a new entry
+  //     // This effectively passes the result object to the entry (and the title and link)
+  //     var entry = new Article(result);
+
+  //     // Now, save that entry to the db
+  //     entry.save(function(err, doc) {
+  //       // Log any errors
+  //       if (err) {
+  //         console.log(err);
+  //       }
+  //       // Or log the doc
+  //       else {
+  //         console.log(doc);
+  //       }
+  //     });
+
+  //   });
+  //       res.send("Scrape Complete");
+
+  // });
+  axios.get("https://tutorialzine.com/articles").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+    $("div.article__description").each(function(i, element) {
       var result = {};
 
-      // Add the title and summary of every link, and save them as properties of the result object
-      // result.title = $(this).children("h2").text();
-      // result.summary = $(this).children(".summary").text();
-      // result.link = $(this).children("h2").children("a").attr("href");
+      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(element)
         .find("h3")
         .text();
@@ -105,25 +142,16 @@ app.get("/scrape", function(req, res) {
         .find("p")
         .text();
 
-      // Using our Article model, create a new entry
-      // This effectively passes the result object to the entry (and the title and link)
-      var entry = new Article(result);
-
-      // Now, save that entry to the db
-      entry.save(function(err, doc) {
-        // Log any errors
-        if (err) {
-          console.log(err);
-        }
-        // Or log the doc
-        else {
-          console.log(doc);
-        }
-      });
-
+      Article.create(result)
+        .then(function(data) {
+          console.log(data);
+        })
+        .catch(function(err) {
+          return res.json(err);
+        });
     });
-        res.send("Scrape Complete");
 
+    res.send("Scrape Complete");
   });
   // Tell the browser that we finished scraping the text
 });
